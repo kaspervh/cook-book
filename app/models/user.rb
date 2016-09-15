b0729a2023e24e0596f37d22e0984a90a6389113
kasper hauschildt <kaspervhauschildt@gmail.com>
2016-09-15 17:29:07 +0200

    made home respond to loged in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ApplicationRecord
-	has_secure_password
-	after_create :default_role
+  has_secure_password
+  after_create :default_role
 
-	def default_role
-		self.role = "student"
-		save!
-	end
+  validates :username, 
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 30 }
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 30 }
+
+  validates :password,
+            length: {minimum: 5}
+
+
+  def default_role
+    self.role = "student"
+    save!
+  end
 end
